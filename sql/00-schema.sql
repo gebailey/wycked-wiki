3e0b0c2f54bb941e49273e4089fc535b0248e923
Greg Bailey <gbailey@lxpro.com>
2019-10-20 21:08:52 -0700

    Add AUTO_INCREMENT to posts schema

--- sql/00-schema.sql
+++ sql/00-schema.sql
@@ -8,7 +8,7 @@ CREATE TABLE `pages` (
 );
 
 CREATE TABLE `posts` (
-  `post_id` int(11) NOT NULL,
+  `post_id` int(11) NOT NULL AUTO_INCREMENT,
   `date` datetime DEFAULT NULL,
   `visible` int(11) DEFAULT '14',
   `title` varchar(30) DEFAULT NULL,
