8dc7b876b3952f966a98bfad5ec391847e0443d5
Greg Bailey <gbailey@lxpro.com>
2019-10-25 05:17:41 -0700

    Version 0.5.1
    - Build docker container with PHP 7.3

--- php/config.php
+++ php/config.php
@@ -7,6 +7,6 @@ $DB_DATABASE = "wycked";
 
 $WYCKED_TITLE = "Wycked Wiki";
 $WYCKED_FOOTER = "Copyright &copy; 1994, Wacky Wild Wycked Wiki WebWorks, Inc.  All Rights Reserved.";
-$WYCKED_VERSION = "0.5.0";
+$WYCKED_VERSION = "0.5.1";
 
 ?>
